9e64d262e0d2d6a18f6ac7895950e8ad9e036004
fileweb380-glitch <webfile380@gmail.com>
2026-05-19 19:40:08 +0300

    fix

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express  = require('express')
 const router   = express.Router()
 const protect  = require('../Middleware/authMiddleware')
-const User     = require('../models/User')
+const User     = require('../Models/User')
 
 // ════════════════════════════════════
 // GET /api/user/me  — get logged-in user profile
